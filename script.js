d0cfe224c3bd1ad8eb42c8e3e956a77bc880e82b
Alireza Sadeghi <alirezasadeghi2300@gmail.com>
2024-11-08 13:27:45 +0300

    first api

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 'use strict';
+// my api key 4lgnZb9buoDcdkr73Zm2niPb5kzo1PoM
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+
+const locations = function () {
+  getPosition()
+    .then(pos => {
+      console.log(pos);
+      const { latitude: lat, longitude: lng } = pos.coords;
+      console.log(lat, lng);
+      return fetch(
+        `https://api.tomtom.com/maps/orbis/places/reverseGeocode/${lat},${lng}.json?key=4lgnZb9buoDcdkr73Zm2niPb5kzo1PoM&radius=100&apiVersion=1`
+      );
+    })
+    .then(res => {
+      console.log(res);
+      return res.json();
+    })
+    .then(data => {
+      console.log(data.addresses[0].address.municipality);
+    });
+};
+
+locations();
