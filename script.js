878f61bd80082e37a85eac0856df580a86d0cbc6
Alireza Sadeghi <alirezasadeghi2300@gmail.com>
2024-11-25 18:04:46 +0300

    adding other lass to js and adding ability to get the city from the input

--- script.js
+++ script.js
@@ -72,6 +72,62 @@ class Data {
   }
 }
 
+class cityData {
+  constructor(city) {
+    this.city = city;
+  }
+  applyWeatherDesign(weatherCondition) {
+    const body = document.body;
+    body.className = '';
+    const weatherClasses = {
+      Clear: 'sunny',
+      Clouds: 'cloudy',
+      Rain: 'rainy',
+      Snow: 'snowy',
+      Drizzle: 'drizzly',
+      Thunderstorm: 'stormy',
+    };
+
+    const weatherClass = weatherClasses[weatherCondition] || 'default-weather';
+    body.classList.add(weatherClass);
+  }
+
+  async location() {
+    try {
+      let location;
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=cea179dffc48165803610302c1237ef4`
+      );
+      if (!res.ok)
+        throw new Error(`problem in getting the location ${res.status}`);
+      console.log(res);
+      const data = await res.json();
+      console.log(data);
+      console.log(data.name);
+      location = data.name;
+      this.applyWeatherDesign(data.weather[0].main);
+      this.renderWeatherInfo(data, location);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  renderWeatherInfo(dataWeather, location) {
+    const page = document.querySelector('.weather-container');
+    page.insertAdjacentHTML('beforeend', WeatherCard(dataWeather, location));
+  }
+}
+const input = document.getElementById('CityInput');
+input.addEventListener('keydown', function (e) {
+  if (e.key === 'Enter') {
+    const CityInput = document.getElementById('CityInput').value;
+    if (CityInput.trim() !== '') {
+      const page = document.querySelector('.weather-container');
+      page.innerHTML = '';
+      const data1 = new cityData(CityInput);
+      data1.location();
+    }
+  }
+});
 const data = new Data();
 data.locations();
 
